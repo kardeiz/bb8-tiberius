b251165d86f2c389ed30bede50e99f7756918419
Jacob Brown <kardeiz@gmail.com>
2019-07-09 17:07:22 -0500

    add run wrapper

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,32 +3,35 @@ use futures_state_stream::StateStream;
 use futures::future::Future;
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let conn_str = std::env::var("DB_CONN")?;
 
-    let fut = futures::future::lazy(|| {
+    let fut = {
+        let conn_str = std::env::var("DB_CONN")?;
 
-        let pool = bb8::Pool::builder()
-            .max_size(2)
-            .build_unchecked(ConnectionManager(conn_str));
+        futures::future::lazy(|| {
 
-        let rt = pool.clone().run(|mut opt_conn| {
-            let conn = opt_conn.unwrap();
-            let rt = conn.simple_query("SELECT @@version")
-                .map_err(|e| (Error::from(e), None))
-                .and_then(|row| {
-                    let val: &str = row.get(0);
-                    Ok(String::from(val))
+            let pool = bb8::Pool::builder()
+                .max_size(2)
+                .build_unchecked(ConnectionManager(conn_str));
+
+            let rt = pool.clone().run(|pooled_conn| {
+                pooled_conn.run(|conn| {
+                    conn.simple_query("SELECT @@version")
+                        .map_err(Error::from)
+                        .map(|row| {
+                            let val: &str = row.get(0);
+                            String::from(val)
+                        })
+                        .collect()
+                        .map(|(items, state)| {
+                            println!("{:?}", &items);
+                            ((), state)
+                        })
                 })
-                .collect()
-                .map(|(items, state)| {
-                    println!("{:?}", &items);
-                    ((), None)
-                });
-            rt
-        });
+            });
 
-        rt
-    });
+            rt
+        })
+    };
 
     tokio::runtime::current_thread::block_on_all(fut).unwrap();
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use futures::future::{self, Future};
+use futures::future::{self, Future, IntoFuture, Either};
 use futures_state_stream::StateStream;
 
 #[derive(Debug, derive_more::Display, derive_more::From)]
 pub enum Error {
-    #[display(fmt = "Tiberius error: {:?}", _0)]
+    #[display(fmt = "Tiberius: {:?}", _0)]
     Tiberius(tiberius::Error),
     #[display(fmt = "Connection removed")]
     EmptyConnection
@@ -12,37 +12,65 @@ pub enum Error {
 #[derive(Debug, Clone)]
 pub struct ConnectionManager(pub String);
 
+pub type SqlConnection = tiberius::SqlConnection<Box<tiberius::BoxableIo>>;
+
 impl bb8::ManageConnection for ConnectionManager {
     /// The connection type this manager deals with.
-    type Connection = Option<tiberius::SqlConnection<Box<tiberius::BoxableIo>>>;
+    type Connection = PooledConnection;
     /// The error type returned by `Connection`s.
     type Error = Error;
 
     /// Attempts to create a new connection.
     fn connect(&self) -> Box<Future<Item = Self::Connection, Error = Self::Error> + Send> {
-        Box::new(tiberius::SqlConnection::connect(&self.0).map(Some).from_err())
+        Box::new(tiberius::SqlConnection::connect(&self.0).map(Some).map(PooledConnection).from_err())
     }
     /// Determines if the connection is still connected to the database.
     fn is_valid(
         &self,
         conn: Self::Connection,
     ) -> Box<Future<Item = Self::Connection, Error = (Self::Error, Self::Connection)> + Send> {
-        match conn {
+        match conn.0 {
             Some(conn) => {
                 let rt = conn
                     .simple_query("SELECT 1").for_each(|_| Ok(()))
                     .from_err()
                     .then(move |r| match r {
-                        Ok(conn) => Ok(Some(conn)),
-                        Err(e) => Err((e, None)),
+                        Ok(conn) => Ok(PooledConnection(Some(conn))),
+                        Err(e) => Err((e, PooledConnection::default())),
                     });
                 Box::new(rt)
             },
-            None => Box::new(future::err((Error::EmptyConnection, None)))
+            None => Box::new(future::err((Error::EmptyConnection, PooledConnection::default())))
         }
     }
     /// Synchronously determine if the connection is no longer usable, if possible.
     fn has_broken(&self, conn: &mut Self::Connection) -> bool {
-        conn.is_none()
+        conn.0.is_none()
+    }
+}
+
+#[derive(Default, derive_more::From)]
+pub struct PooledConnection(pub Option<SqlConnection>);
+
+impl PooledConnection {
+    pub fn run<'a, T, E, U, F>(
+        self,
+        f: F,
+    ) -> impl Future<Item = (T, Self), Error = (E, Self)> + Send + 'a
+    where
+        F: FnOnce(SqlConnection) -> U + Send + 'a,
+        U: IntoFuture<Item = (T, SqlConnection), Error = E> + Send + 'a,
+        U::Future: Send + 'a,
+        E: From<Error> + Send + 'a,
+        T: Send + 'a,
+    {
+        match self.0 {
+            Some(conn) => Either::A(f(conn)
+                .into_future()
+                .map(|(t, conn)| (t, PooledConnection(Some(conn))))
+                .map_err(|e| (e.into(), PooledConnection::default()))),
+            None => 
+                Either::B(future::err((Error::EmptyConnection.into(), PooledConnection::default())))
+        }
     }
 }
