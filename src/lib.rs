6e8e8069adaaa3b937c20341db9fdf078ba23916
Chen Xu <windoze@0d0a.com>
2021-05-08 02:48:50 +0800

    Support AzureSQL redirect

--- src/lib.rs
+++ src/lib.rs
@@ -71,10 +71,33 @@ pub mod rt {
             use tokio_util::compat::TokioAsyncWriteCompatExt;//Tokio02AsyncWriteCompatExt;
 
             let tcp = TcpStream::connect(self.config.get_addr()).await?;
+            tcp.set_nodelay(true)?;
 
             (self.modify_tcp_stream)(&tcp)?;
 
-            let client = tiberius::Client::connect(self.config.clone(), tcp.compat_write()).await?;
+            let client = match Client::connect(self.config.clone(), tcp.compat_write()).await {
+                // Connection successful.
+                Ok(client) => client,
+
+                // The server wants us to redirect to a different address
+                Err(tiberius::error::Error::Routing { host, port }) => {
+                    let mut config = self.config.clone();
+
+                    config.host(&host);
+                    config.port(port);
+
+                    let tcp = TcpStream::connect(config.get_addr()).await?;
+                    tcp.set_nodelay(true)?;
+
+                    (self.modify_tcp_stream)(&tcp)?;
+
+                    // we should not have more than one redirect, so we'll short-circuit here.
+                    tiberius::Client::connect(config, tcp.compat_write()).await?
+                }
+
+                // Other error happened
+                Err(e) => Err(e)?,
+            };
 
             Ok(client)
         }
@@ -99,10 +122,34 @@ pub mod rt {
 
         pub(crate) async fn connect_inner(&self) -> Result<Client, super::Error> {
             let tcp = async_std::net::TcpStream::connect(self.config.get_addr()).await?;
+            tcp.set_nodelay(true)?;
 
             (self.modify_tcp_stream)(&tcp)?;
 
-            let client = tiberius::Client::connect(self.config.clone(), tcp).await?;
+            // let client = tiberius::Client::connect(self.config.clone(), tcp).await?;
+            let client = match Client::connect(self.config.clone(), tcp).await {
+                // Connection successful.
+                Ok(client) => client,
+
+                // The server wants us to redirect to a different address
+                Err(tiberius::error::Error::Routing { host, port }) => {
+                    let mut config = self.config.clone();
+
+                    config.host(&host);
+                    config.port(port);
+
+                    let tcp = async_std::net::TcpStream::connect(config.get_addr()).await?;
+                    tcp.set_nodelay(true)?;
+
+                    (self.modify_tcp_stream)(&tcp)?;
+
+                    // we should not have more than one redirect, so we'll short-circuit here.
+                    tiberius::Client::connect(config, tcp).await?
+                }
+
+                // Other error happened
+                Err(e) => Err(e)?,
+            };
 
             Ok(client)
         }
